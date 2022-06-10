adf7ccfe5cc78d94c66a53773768abcad163f57f
anjaemin <t01089416422@gmail.com>
2022-06-10 20:35:24 +0900

    제작중:

--- src/components/Object/Link/index.tsx
+++ src/components/Object/Link/index.tsx
@@ -15,10 +15,12 @@ type LinkABodyType = {
     color:string,
 };
 
-const LinkA: React.FC<LinkAType> = ({size,text,href,bold,color }) =>{
+//hover boolean (hover 넣을것인가?),hover color(hover 색?),hover effect(hover 애니메이션?) 추가 예정
+
+const LinkA: React.FC<LinkAType> = ({size,text,href,bold,color }) =>{  
     return <LinkABody  href={href} size={size} bold={bold} color={color}> {text} </LinkABody>
 }
-
+ 
 
 const LinkABody = styled.a<LinkABodyType>`
     font-size:${(props) => props.size};
